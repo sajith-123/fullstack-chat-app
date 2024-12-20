afbe9e2ddbbdc0702cfdef08025686d3fd3384f1
Sajith Salu <sajithsalu06@gmail.com>
2024-12-20 12:41:03 +0530

    project completed

---
+++ backend/src/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import path from "path";
+import cookieParser from "cookie-parser";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+// Middleware
+app.use(express.json({ limit: "50mb" })); // Adjust "50mb" or set as needed
+app.use(express.urlencoded({ extended: true, limit: "50mb" })); // For URL-encoded data
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist"));
+  });
+}
+// Server start and DB connection
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
