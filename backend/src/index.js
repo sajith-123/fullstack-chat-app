fdbeac98605f26c99e121ed45e887c3c8af38ec6
sajith-123 <sajithsalu06@gmail.com>
2024-12-20 13:36:43 +0530

    index,js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import path from "path";
-import cookieParser from "cookie-parser";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
@@ -14,9 +16,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// Middleware
-app.use(express.json({ limit: "50mb" })); // Adjust "50mb" or set as needed
-app.use(express.urlencoded({ extended: true, limit: "50mb" })); // For URL-encoded data
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
@@ -25,7 +25,6 @@ app.use(
   })
 );
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
@@ -36,8 +35,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-// Server start and DB connection
+
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
